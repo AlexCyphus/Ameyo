0776b96125562bc9c56c1cf36e0f2bee9c4acbb5
Alex Cyphus <alexjcyphus@gmail.com>
2020-10-25 09:55:31 +0100

    Working history view

--- src/functions/queryLogic.js
+++ src/functions/queryLogic.js
@@ -5,6 +5,7 @@ export function queryLocalStorage(_callback = false){
     if (JSON.parse(localStorage.getItem('columns'))){newState.columns = JSON.parse(localStorage.getItem('columns'))}
     if (JSON.parse(localStorage.getItem('columnOrder'))){newState.columnOrder = JSON.parse(localStorage.getItem('columnOrder'))}
     if (JSON.parse(localStorage.getItem('monthlyHabitsCount'))){newState.monthlyHabitsCount = JSON.parse(localStorage.getItem('monthlyHabitsCount'))}
+    if (JSON.parse(localStorage.getItem('history'))){newState.history = JSON.parse(localStorage.getItem('history'))}
 
     this.setState(newState, () => {
         if (_callback) {_callback()};
