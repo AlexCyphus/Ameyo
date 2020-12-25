6ee5245849917ca08a8da32b7e107ca6e9b41fcb
Alex Cyphus <alexjcyphus@gmail.com>
2020-12-25 09:53:51 -0800

    Fix potential infinite render loop

--- src/Item.js
+++ src/Item.js
@@ -3,32 +3,46 @@ import {Draggable} from 'react-beautiful-dnd'
 import Label from './Label.tsx'
 
 export default class Item extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      label: false,
+      color: 'none'
+    }
+  }
 
-  render() {
-    const itemId = this.props.item.id;
-    const checkedClass = this.props.checked;
-    var label = false
-    var color;
-
-    // check if item should have label 
-    if (this.props.item.content.split(" ")[0].includes(":")){label = this.props.item.content.split(":")[0]}
-
+  UNSAFE_componentWillMount(){
+    if (this.props.item.content.split(" ")[0].includes(":")){this.state.label = this.props.item.content.split(":")[0]}
     if (this.props.colors && Object.values(this.props.colors)){
-      if (!Object.values(this.props.colors).includes(label)){
+      if (!Object.values(this.props.colors).includes(this.state.label)){
         for (var x = 0; x < Object.keys(this.props.colors).length; x++){
           let key = Object.keys(this.props.colors)[x]
           let val = this.props.colors[key]
           if (val === false){
-            color = key
-            this.props.claimColor(key, label)
+            this.setState({color: key})
+            // this is where the error comes from
+            this.props.claimColor(key, this.state.label)
             break;
           }
         }
       }
-      else {
-        color = Object.keys(this.props.colors).find(key => this.props.colors[key] === label)
-      }
+      else {this.setState({color: Object.keys(this.props.colors).find(key => this.props.colors[key] === this.state.label)})}
     }
+  }
+  
+  
+  render() {
+    const itemId = this.props.item.id;
+    const checkedClass = this.props.checked;
+
+    /* UNRESOLVED ISSUE */
+    /* 
+    We use the "colors" state to determine what colors have already been taken by other labels
+    We claim these colors on rendering which causes the state to change, the component to re-render, and _theoretically_ the function to be called again
+    Need to move the this.props.claimColor outside of the render method
+    */
+
+    // check if item should have label 
 
     return (
       <Draggable draggableId={itemId} index={this.props.index} id={itemId}>
@@ -44,7 +58,7 @@ export default class Item extends React.Component {
             <div className={"item-name"} id={itemId}>
               <p className={"item-p " + (this.props.type !== 'none' ? 'px-3' : '')} id={itemId}>{this.props.item.content}</p>
             </div>
-            <Label display={label} color={String(color)}/>
+            <Label display={this.state.label} color={String(this.state.color)}/>
           </div>
         )}
       </Draggable>
